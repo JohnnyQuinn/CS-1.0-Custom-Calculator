b62e2af2096339f481f3250e09ae70ca7565fede
johnnyquinn <jftq97@gmail.com>
2020-09-01 06:57:03 -0400

    Changes some string outputs

--- Custom-calculator.py
+++ Custom-calculator.py
@@ -9,11 +9,11 @@ CONSTANT = 5252
 #getting user input for engine speed and torque
 print("\nFind out the max horsepower output of an engine.")
 engine_speed = int(input("What is the max engine speed in RPMs?:\n"))
-engine_torque = int(input("What is the max engine torque?:\n"))
+engine_torque = int(input("What is the max engine torque in lb/ft?:\n"))
 
 #performing calculation
 horse_power = int((engine_speed * engine_torque)/CONSTANT)
 
 #print result
-print(f"This engine's max horsepower output about {horse_power} HP at {engine_speed} RPMs with {engine_torque} lbs of torque")
+print(f"This engine's max horsepower output about {horse_power} HP @ {engine_speed} RPMs with {engine_torque} lbs of torque\n")
 
